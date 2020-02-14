4e0e93ba17ade3908edf8f54abe17b6e93ee04bb
Mario Patronelli <mario@raffles.com.ar>
2020-02-14 12:49:18 -0300

    Agrega opción para Anular pagares pendientes. Agrega helpers con metodo formatMoney

---
+++ Resources/Javascript/utilities/helpers.js
@@ -0,0 +1,13 @@
+export function formatMoney(currency, value) {
+    if (currency === "PYG" || currency === "Gs" || currency === "Guaraní") {
+         return Number(value).toLocaleString("es-PY", {
+            style: "currency",
+            currency: "PYG"
+        })
+    } else if (currency === "USD") {
+        return Number(value).toLocaleString("en-USD", {
+            style: "currency",
+            currency: "USD"
+        })
+    }
+}
