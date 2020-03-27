26d82b80a7205697170ecf86357639839f8ec74a
Mario Patronelli <mario@raffles.com.ar>
2020-03-26 23:47:05 -0400

    Remueve propiedad innecesaria

--- Models/User.php
+++ Models/User.php
@@ -9,24 +9,6 @@ use Caffeinated\Shinobi\Models\Role;
 class User extends Model
 {
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'bloqueado',
-        'codigo_validacion',
-        'email',
-        'first_name',
-        'id_persona',
-        'last_name',
-	'password',
-	'provider',
-	'provider_id',
-        'role_id',    
-    ];
-
-    /**
      * The relationships that should always be loaded.
      *
      * @var array
