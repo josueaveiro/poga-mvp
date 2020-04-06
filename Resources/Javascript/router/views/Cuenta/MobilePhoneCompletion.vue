b96d309a6348398ac1107a0e5db1cf612fc90008
Mario Patronelli <mario@raffles.com.ar>
2020-04-06 14:12:48 -0400

    Vista MobilePhoneCompletionForm.vue

---
+++ Resources/Javascript/router/views/Cuenta/MobilePhoneCompletion.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="m-1 m-md-4">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <div class="box">
+          <MobilePhoneCompletionForm />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
